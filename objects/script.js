d4ec21ec4e705bbe4236323a4592ee54105ed08b
suraj_122 <122suraj0@gmail.com>
2019-02-03 20:51:39 +0530

    more in objects

--- objects/script.js
+++ objects/script.js
@@ -1,29 +1,29 @@
 'use strict';
 
-// let user = new Object(); //"object constructor" syntax
-// let user = {}; //"object literal" syntax
+let user = new Object(); //"object constructor" syntax
+let user = {}; //"object literal" syntax
 
-// let user = {        // an object
-// 	name: "John",     // key "name" with value "John" 
-// 	age: "30",         // key "age" with value "30"
-// };
+let user = {        // an object
+	name: "John",     // key "name" with value "John" 
+	age: "30",         // key "age" with value "30"
+};
 
-// alert(user.name);
-// alert(user.age);
+alert(user.name);
+alert(user.age);
 
-// user.isAdmin = true;
+user.isAdmin = true;
 
-// delete user.age;
-// user["likes bird"] = true;
-// alert(user["likes bird"]);
+delete user.age;
+user["likes bird"] = true;
+alert(user["likes bird"]);
 
-// let currentUser = {
-// 	name: "Joseph",
-// 	age: 40,
-// }
-// let key = prompt("what do you want to know about user?", "name");
+let currentUser = {
+	name: "Joseph",
+	age: 40,
+}
+let key = prompt("what do you want to know about user?", "name");
 
-// alert(currentUser[key]);
+alert(currentUser[key]);
 
 // Computed properties
 
@@ -34,3 +34,53 @@ let bag = {
 }
 
 alert(bag.orangeComputers);
+
+let obj = {};
+obj._proto_ = 5;
+alert(obj.__proto__);
+
+// property value shorthand
+function makeUser(name, age) {
+	return {
+		// name: name,
+		// age: age,
+		name,
+		age,
+	};
+}
+
+let user = makeUser("John", 40);
+let user = {
+	name,
+	age: 30,
+}
+alert(user.age);
+
+// Existence check
+// let person = {};
+
+
+let person = {name: "John", age: "30"};
+
+alert("age" in person);
+alert("blabla" in person);
+alert(person.noSuchProperty === undefined );
+
+let key = "age";
+alert(key in person);
+
+let obj = {
+	test: undefined
+};
+
+alert(obj.test);
+
+alert( "test" in obj);
+
+
+
+
+
+
+
+
